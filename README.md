79b18f9ca6bfe9627f61a81a3b7115689bf251de
Rostyslav <hasiukrostyslav@gmail.com>
2023-01-10 21:15:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Planning Meeting Calendar App"
+
+Meeting calendar app for a meeting room in your office:
+
+- Display meeting planned;
+
+* Filter meetings for a particular team member;
+
+- Add new meetings;
+
+* Delete meetings;
+
+---
+
+### Technologies
+
+- `HTML`;
+- `CSS/SASS preprocessor (SCSS)`;
+- `JavaScript` with Babel transpiler lintered with `airbnb/javascript` style guide
+- `Webpack module bundler`;
+
+### Setup
+
+To run this project, install it locally using npm:
+
+```
+$ git https://github.com/hasiukrostyslav/planningMeetingCalendar.git
+$ npm install
+$ npm run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/scripts/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: 'assets/[name].[ext]',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 3000,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.woff2?$|ttf/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].ext',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: 'Calendar',
+      template: 'src/index.html',
+      minify: false,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'event.html',
+      title: 'Create event',
+      template: 'src/event.html',
+      minify: false,
+    }),
+  ],
+};
